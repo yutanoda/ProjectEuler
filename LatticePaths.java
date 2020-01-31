1f91cb087a03efd97c43d933a028f2962f6d4b90
yutanoda <atuyuta902@yahoo.co.jp>
2020-01-31 11:53:55 +0900

    no15

---
+++ LatticePaths.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String args[]) {
+		int gridSize = 20;
+		long paths = 1;
+		 
+		for (int i = 0; i < gridSize; i++) {
+			paths *= (2 * gridSize) - i;
+			paths /= i + 1;
+		}
+		 System.out.println(paths);
+	}
+}
